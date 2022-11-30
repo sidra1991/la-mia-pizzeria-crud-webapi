24ed1fedb651fc1f51e7ffe22b2d878ca047e37c
anthony cecconi <anthony.cecconi.91@gmail.com>
2022-11-30 15:05:12 +0100

    lista messaggi operativa sulla vista. dopo la pausa passo al form

--- Controllers/API/ApiMessageController.cs
+++ Controllers/API/ApiMessageController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace la_mia_pizzeria_static.Controllers.message
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ApiMessageController : ControllerBase
     {
