3ba18c8f04960a14a0ed6a3b8132387c73db4c06
anthony cecconi <anthony.cecconi.91@gmail.com>
2022-11-30 17:15:21 +0100

    trovato come rispettare il bonus

--- Models/message.cs
+++ Models/message.cs
@@ -1,13 +1,18 @@
-﻿using System.Runtime.ConstrainedExecution;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.ConstrainedExecution;
 
 namespace la_mia_pizzeria_static.Models
 {
     public class Message
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "questo campo è obligatorio")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "questo campo è obligatorio")]
         public string TitleMessage { get; set; }
+        [Required(ErrorMessage = "questo campo è obligatorio")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "questo campo è obligatorio")]
         public string TextMessage { get; set; }
 
     }
