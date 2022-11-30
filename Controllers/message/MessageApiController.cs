21fbcef14dc2095ed2ce62ece1acf65366ee8b45
anthony cecconi <anthony.cecconi.91@gmail.com>
2022-11-30 13:52:52 +0100

    creati controller e viste

---
+++ Controllers/message/MessageApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers.message
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageApiController : ControllerBase
+    {
+    }
+}
