9c4262404e957e3855ba23b537be2ed9d2ec87fd
anthony cecconi <anthony.cecconi.91@gmail.com>
2022-11-30 13:48:35 +0100

    classe messaggi creata

---
+++ Models/message.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class message
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string TitleMessage { get; set; }
+        public string Name { get; set; }
+        public string TextMessage { get; set; }
+
+    }
+}
